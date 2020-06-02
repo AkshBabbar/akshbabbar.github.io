89a96d7b53148b73dd6b636a819b26c105be390a
AkshBabbar <35312661+AkshBabbar@users.noreply.github.com>
2020-06-03 03:34:48 +0530

    Basic Lab requirements completed

--- Labs/Lab2/scripts/groceries.js
+++ Labs/Lab2/scripts/groceries.js
@@ -1,56 +1,106 @@
-	
-// Array of products, each product is an object with different fieldset
-// A set of ingredients should be added to products		 
+	// Array of products, each product is an object with different fieldset
+	// A set of ingredients should be added to products		 
 
-var products = [
-	{
-		name: "brocoli",
-		vegetarian: true,
-		glutenFree: true,
-		price: 1.99
-	},
-	{
-		name: "bread",
-		vegetarian: true,
-		glutenFree: false,
-		price: 2.35
-	},
-	{
-		name: "salmon",
-		vegetarian: false,
-		glutenFree: true,
-		price: 10.00
+	var products = [{
+	        name: "Brocoli",
+	        vegetarian: true,
+	        glutenFree: true,
+	        organic: true,
+	        price: 1.99
+	    },
+	    {
+	        name: "Spinach",
+	        vegetarian: true,
+	        glutenFree: true,
+	        organic: true,
+	        price: 2.99
+	    },
+	    {
+	        name: "Bread",
+	        vegetarian: true,
+	        glutenFree: false,
+	        organic: false,
+	        price: 2.35
+	    },
+	    {
+	        name: "Eggs",
+	        vegetarian: false,
+	        glutenFree: true,
+	        organic: false,
+	        price: 2.35
+	    },
+	    {
+	        name: "Milk",
+	        vegetarian: true,
+	        glutenFree: true,
+	        organic: false,
+	        price: 4.00
+	    },
+	    {
+	        name: "Bananas",
+	        vegetarian: true,
+	        glutenFree: true,
+	        organic: true,
+	        price: 2.00
+	    },
+	    {
+	        name: "Cake",
+	        vegetarian: true,
+	        glutenFree: true,
+	        organic: false,
+	        price: 10.99
+	    },
+	    {
+	        name: "Salmon",
+	        vegetarian: false,
+	        glutenFree: true,
+	        organic: false,
+	        price: 10.00
+	    },
+	    {
+	        name: "Chicken",
+	        vegetarian: false,
+	        glutenFree: true,
+	        organic: true,
+	        price: 14.99
+	    },
+	    {
+	        name: "Orange Juice",
+	        vegetarian: true,
+	        glutenFree: true,
+	        organic: true,
+	        price: 7.00
+	    }
+	];
+
+
+
+	// given restrictions provided, make a reduced list of products
+	// prices should be included in this list, as well as a sort based on price
+
+	function restrictListProducts(prods, restriction) {
+	    let product_names = [];
+	    for (let i = 0; i < prods.length; i += 1) {
+	        if ((restriction.includes("Vegetarian")) && (prods[i].vegetarian == true)) {
+	            product_names.push(prods[i].name);
+	        } else if ((restriction.includes("GlutenFree")) && (prods[i].glutenFree == true)) {
+	            product_names.push(prods[i].name);
+	        } else if ((restriction.includes("Organic")) && (prods[i].organic == true)) {
+	            product_names.push(prods[i].name);
+	        } else if (restriction.includes("None")) {
+	            product_names.push(prods[i].name);
+	        }
+	    }
+	    return product_names;
 	}
-];
-	
 
-
-// given restrictions provided, make a reduced list of products
-// prices should be included in this list, as well as a sort based on price
-
-function restrictListProducts(prods, restriction) {
-	let product_names = [];
-	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name);
-		}
-		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name);
-		}
-		else if (restriction == "None"){
-			product_names.push(prods[i].name);
-		}
+	// Calculate the total price of items, with received parameter being a list of products
+	function getTotalPrice(chosenProducts) {
+	    totalPrice = 0;
+	    for (let i = 0; i < products.length; i += 1) {
+	        if (chosenProducts.indexOf(products[i].name) > -1) {
+	            totalPrice += products[i].price;
+	        }
+	    }
+	    return totalPrice;
 	}
-	return product_names;
-}
-
-// Calculate the total price of items, with received parameter being a list of products
-function getTotalPrice(chosenProducts) {
-	totalPrice = 0;
-	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name) > -1){
-			totalPrice += products[i].price;
-		}
-	}
-	return totalPrice;
-}
