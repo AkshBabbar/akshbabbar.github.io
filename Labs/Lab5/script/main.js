2eb5b305c9a4d7eb741fcfeb3e2bd857af12c9b0
AkshBabbar <35312661+AkshBabbar@users.noreply.github.com>
2020-06-21 00:02:57 +0530

    Jquery updates

--- Labs/Lab5/script/main.js
+++ Labs/Lab5/script/main.js
@@ -1,7 +1,48 @@
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(45.382740, -75.695900),
-        zoom: 5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+var total = 0;
+var services = [];
+
+$(".select-service").click(function() {
+    var servicePrice = parseInt($(this).parent().children(".price").children().html());
+    if ($(this).hasClass("selected") == false) {
+        $(this).html('<i class="far fa-check-circle"></i> Selected').addClass("selected");
+        $(this).parent().parent().addClass("service-card-selected");
+        total = total + servicePrice;
+        services.push($(this).parent().children("h4").text());
+    } else {
+        $(this).html("Select Service").removeClass("selected");
+        $(this).parent().parent().removeClass("service-card-selected");
+        total = total - servicePrice;
+        services.splice(services.indexOf($(this).parent().children("h4").text()), 1);
+    }
+    $("#reqd-service").val("Services: " + services);
+});
+
+$(".barbers").click(function() {
+    var text = $(this).children(".barber-info").children("h4").text();
+    $("#reqd-expert").val("Expert: " + text);
+});
+
+$("#reqd-service").click(function() {
+    if ($(this).val() == "") {
+        $('html, body').animate({
+            scrollTop: $("#services").offset().top
+        }, 500);
+    }
+});
+
+$("#reqd-expert").click(function() {
+    if ($(this).val() == "") {
+        $('html, body').animate({
+            scrollTop: $("#experts").offset().top
+        }, 500);
+    }
+});
+
+$(".nav-item.nav-link").click(function() {
+    $(".nav-item.nav-link").removeClass("active");
+    $(this).addClass("active");
+});
+
+function finalServices() {
+
 }
